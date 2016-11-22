2986439c5c465840b80e70cb3fa31e9a654da661
JS Morisset <jsm@surniaulula.com>
2016-11-22 10:21:44 -0800

    v1.0.3-rc1

--- lib/config.php
+++ lib/config.php
@@ -15,7 +15,7 @@ if ( ! class_exists( 'WpssoSsmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssossm' => array(
-					'version' => '1.0.3-dev2',		// plugin version
+					'version' => '1.0.3-rc1',		// plugin version
 					'opt_version' => '1',		// increment when changing default options
 					'short' => 'WPSSO SSM',		// short plugin name
 					'name' => 'WPSSO Strip Schema Microdata (WPSSO SSM)',
@@ -28,7 +28,7 @@ if ( ! class_exists( 'WpssoSsmConfig' ) ) {
 					'req' => array(
 						'short' => 'WPSSO',
 						'name' => 'WordPress Social Sharing Optimization (WPSSO)',
-						'min_version' => '3.37.4-dev2',
+						'min_version' => '3.37.4-rc1',
 					),
 					'img' => array(
 						'icon_small' => 'images/icon-128x128.png',
