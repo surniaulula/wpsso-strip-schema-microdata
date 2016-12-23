806f7602e16af595a6a9fb456c6315899602c375
JS Morisset <jsm@surniaulula.com>
2016-12-22 17:47:36 -0800

    v1.0.3-1

--- wpsso-strip-schema-microdata.php
+++ wpsso-strip-schema-microdata.php
@@ -38,11 +38,11 @@ if ( ! class_exists( 'WpssoSsm' ) ) {
 		public $reg;			// WpssoSsmRegister
 		public $filters;		// WpssoSsmFilters
 
-		private static $instance = null;
+		private static $instance;
 		private static $have_min = true;
 
 		public static function &get_instance() {
-			if ( self::$instance === null )
+			if ( ! isset( self::$instance ) )
 				self::$instance = new self;
 			return self::$instance;
 		}
