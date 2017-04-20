569d44d673b65087184aea6af7bc8155a42a80f1
JS Morisset <jsm@surniaulula.com>
2017-04-20 20:11:34 +0200

    v1.1.6

--- wpsso-strip-schema-microdata.php
+++ wpsso-strip-schema-microdata.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO extension to remove outdated / incomplete Schema Microdata, leaving the Google recommended Schema JSON-LD markup untouched.
  * Requires At Least: 3.7
- * Tested Up To: 4.7.3
+ * Tested Up To: 4.7.4
  * Version: 1.1.6
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
