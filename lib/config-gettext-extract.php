7593ae8e5ee5f979726d8f3e5aa737fb18efb025
JS Morisset <jsm@surniaulula.com>
2017-09-30 11:09:12 -0400

    v1.2.3

--- lib/config-gettext-extract.php
+++ lib/config-gettext-extract.php
@@ -1,5 +1,5 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
-// extract of translatable text strings from static config array(s)
+// extract of translatable text strings from static config array
 _x( 'Strip Microdata', 'lib file description', 'wpsso-strip-schema-microdata' );
 _x( 'WPSSO extension to remove outdated / incomplete Schema Microdata, leaving the Google recommended Schema JSON-LD markup untouched.', 'plugin description', 'wpsso-strip-schema-microdata' );
 ?>
