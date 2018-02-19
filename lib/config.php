8cba8ab0c13d723bda0b860038836f0e2a5287b7
JS Morisset <jsm@surniaulula.com>
2018-02-19 08:09:21 -0600

    v1.2.7

--- lib/config.php
+++ lib/config.php
@@ -27,7 +27,7 @@ if ( ! class_exists( 'WpssoSsmConfig' ) ) {
 					'text_domain' => 'wpsso-strip-schema-microdata',
 					'domain_path' => '/languages',
 					'req' => array(
-						'short' => 'WPSSO',
+						'short' => 'WPSSO Core',
 						'name' => 'WPSSO Core',
 						'min_version' => '3.52.0',
 					),
