e01c346eb99afa4884842626efc0953b054547aa
JS Morisset <jsm@surniaulula.com>
2018-04-03 22:21:28 -0400

    v1.3.2-dev.1

--- wpsso-strip-schema-microdata.php
+++ wpsso-strip-schema-microdata.php
@@ -13,7 +13,7 @@
  * Description: WPSSO Core add-on to remove outdated / incomplete Schema Microdata, leaving the Google recommended Schema JSON-LD markup untouched.
  * Requires PHP: 5.4
  * Requires At Least: 3.8
- * Tested Up To: 4.9.4
+ * Tested Up To: 4.9.5
  * Version: 1.3.2-dev.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
