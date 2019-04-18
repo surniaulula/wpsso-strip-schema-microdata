58055fb951097b0190e2778b91b96f3d22d7011c
JS Morisset <jsm@surniaulula.com>
2019-04-17 20:58:25 -0700

    v1.4.3

--- lib/gettext-config.php
+++ lib/gettext-config.php
@@ -4,4 +4,4 @@
  * Extract of translatable text strings from the static config array.
  */
 _x( 'Strip Microdata', 'lib file description', 'wpsso-strip-schema-microdata' );
-_x( 'WPSSO Core add-on to remove outdated or incomplete Schema Microdata to use Google\'s preferred Schema JSON-LD / Rich Card markup.', 'plugin description', 'wpsso-strip-schema-microdata' );
+_x( 'WPSSO Core add-on to remove outdated / incomplete Schema Microdata to use Google\'s preferred Schema JSON-LD for Rich Results and SEO.', 'plugin description', 'wpsso-strip-schema-microdata' );
