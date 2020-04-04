261ce742a4911745ba4c7b3fc82c9571157c83ee
JS Morisset <jsm@surniaulula.com>
2020-04-04 12:13:52 -0700

    v2.1.0-dev.4

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoSsmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssossm' => array(			// Plugin acronym.
-					'version'     => '2.1.0-dev.3',	// Plugin version.
+					'version'     => '2.1.0-dev.4',	// Plugin version.
 					'opt_version' => '3',		// Increment when changing default option values.
 					'short'       => 'WPSSO SSM',	// Short plugin name.
 					'name'        => 'WPSSO Strip Schema Microdata',
@@ -34,7 +34,7 @@ if ( ! class_exists( 'WpssoSsmConfig' ) ) {
 						'wpsso' => array(
 							'class'       => 'Wpsso',
 							'name'        => 'WPSSO Core',
-							'min_version' => '6.27.2-dev.3',
+							'min_version' => '6.28.0-dev.4',
 						),
 					),
 
--- wpsso-strip-schema-microdata.php
+++ wpsso-strip-schema-microdata.php
@@ -12,9 +12,9 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Removes outdated Schema microdata from theme templates and leaves Schema JSON-LD untouched for better Google Rich Results.
  * Requires PHP: 5.6
- * Requires At Least: 4.0
+ * Requires At Least: 4.2
  * Tested Up To: 5.4
- * Version: 2.1.0-dev.3
+ * Version: 2.1.0-dev.4
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
