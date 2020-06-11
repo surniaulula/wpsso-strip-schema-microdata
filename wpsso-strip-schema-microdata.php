5a7208c92876b40a2fe29fa253da51309774a45a
JS Morisset <jsm@surniaulula.com>
2020-06-11 09:11:13 -0700

    v2.3.1

--- wpsso-strip-schema-microdata.php
+++ wpsso-strip-schema-microdata.php
@@ -13,7 +13,7 @@
  * Description: Remove Outdated / Incorrect Microdata from Theme Templates for Better Google Rich Results with Schema JSON-LD Markup.
  * Requires PHP: 5.6
  * Requires At Least: 4.2
- * Tested Up To: 5.4.1
+ * Tested Up To: 5.4.2
  * Version: 2.3.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
