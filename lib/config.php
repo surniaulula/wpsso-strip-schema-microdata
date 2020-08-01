a187737103525802f127eba31eaf7c03bf366cb9
JS Morisset <jsm@surniaulula.com>
2020-08-01 09:36:06 -0700

    v2.5.0-dev.3

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoSsmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssossm' => array(			// Plugin acronym.
-					'version'     => '2.5.0-dev.2',	// Plugin version.
+					'version'     => '2.5.0-dev.3',	// Plugin version.
 					'opt_version' => '3',		// Increment when changing default option values.
 					'short'       => 'WPSSO SSM',	// Short plugin name.
 					'name'        => 'WPSSO Strip Schema Microdata',
@@ -36,7 +36,7 @@ if ( ! class_exists( 'WpssoSsmConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '7.15.0-dev.2',
+							'min_version'   => '7.15.0-dev.3',
 						),
 					),
 
@@ -45,8 +45,8 @@ if ( ! class_exists( 'WpssoSsmConfig' ) ) {
 					 */
 					'assets' => array(
 						'icons' => array(
-							'1x' => 'images/icon-128x128.png',
-							'2x' => 'images/icon-256x256.png',
+							'low'  => 'images/icon-128x128.png',
+							'high' => 'images/icon-256x256.png',
 						),
 					),
 
