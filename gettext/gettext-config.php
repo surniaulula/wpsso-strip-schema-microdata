b3cdddbf434fd1c13ad7851d616b74ec57f3cf91
JS Morisset <jsm@surniaulula.com>
2020-09-13 11:05:20 -0700

    v2.6.0

---
+++ gettext/gettext-config.php
@@ -0,0 +1,7 @@
+<?php die( 'These aren\'t the droids you\'re looking for.' );
+
+/**
+ * Extract of translatable strings from the config array.
+ */
+_x( 'Strip Microdata', 'lib file description', 'wpsso-strip-schema-microdata' );
+_x( 'Remove Outdated / Incorrect Microdata from Theme Templates for Better Google Rich Results with Schema JSON-LD Markup.', 'plugin description', 'wpsso-strip-schema-microdata' );
