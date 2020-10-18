a98ef15892394c8d53cb8fcac7305bdc1f547fc6
JS Morisset <jsm@surniaulula.com>
2020-10-18 11:14:03 -0700

    v2.8.1

--- wpsso-strip-schema-microdata.php
+++ wpsso-strip-schema-microdata.php
@@ -66,7 +66,10 @@ if ( ! class_exists( 'WpssoSsm' ) ) {
 			load_plugin_textdomain( 'wpsso-strip-schema-microdata', false, 'wpsso-strip-schema-microdata/languages/' );
 		}
 
-		public function init_objects( $is_admin ) {
+		/**
+		 * $is_admin, $doing_ajax, and $doing_cron available since WPSSO Core v8.8.0.
+		 */
+		public function init_objects( $is_admin = false, $doing_ajax = false, $doing_cron = false ) {
 
 			$this->p =& Wpsso::get_instance();
 
