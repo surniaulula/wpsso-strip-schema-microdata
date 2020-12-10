fabdccd8048cf864cb14de81c11f681c99ab9103
JS Morisset <jsm@surniaulula.com>
2020-12-10 11:44:47 -0800

    v2.10.0

--- lib/filters-messages.php
+++ lib/filters-messages.php
@@ -60,39 +60,39 @@ if ( ! class_exists( 'WpssoSsmFiltersMessages' ) ) {
 			if ( isset( $section ) ) {	// Just in case.
 
 				switch ( $msg_key ) {
-	
+
 					case 'tooltip-ssm_body_section_meta_tags':
 					case 'tooltip-ssm_head_section_meta_tags':
-	
+
 						$text = sprintf( __( 'Remove known duplicate / conflicting meta tags from the webpage %1$s section.',
 							'wpsso-strip-schema-microdata' ), '<code>&amp;lt;' . $section . '&amp;gt;</code>' ) . ' ';
-	
+
 						$text .= $def_msg;
-	
+
 						break;
-	
+
 					case 'tooltip-ssm_body_section_json_scripts':
 					case 'tooltip-ssm_head_section_json_scripts':
-	
+
 						$text = sprintf( __( 'Remove <code>application/ld+json</code> scripts from the webpage %1$s section.',
 							'wpsso-strip-schema-microdata' ), '<code>&amp;lt;' . $section . '&amp;gt;</code>' ) . ' ';
-	
+
 						$text .= $def_msg;
-	
+
 						break;
-	
+
 					case 'tooltip-ssm_body_section_schema_attr':
 					case 'tooltip-ssm_head_section_schema_attr':
-	
+
 						$text = sprintf( __( 'Remove Schema HTML attributes (itemscope, itemtype, itemprop, etc.) from the webpage %1$s section.',
 							'wpsso-strip-schema-microdata' ), '<code>&amp;lt;' . $section . '&amp;gt;</code>' ) . ' ';
-	
+
 						$text .= $def_msg;
-	
+
 						break;
 				}
 			}
-	
+
 			return $text;
 		}
 	}
