e290df985a54ff0a5facc32a572bedea9faaf49f
JS Morisset <jsm@surniaulula.com>
2021-07-07 07:28:20 -0700

    v2.11.2-b.2

--- lib/filters.php
+++ lib/filters.php
@@ -170,7 +170,7 @@ if ( ! class_exists( 'WpssoSsmFilters' ) ) {
 
 					$mt_placeholder = '<!-- placeholder for ' . WPSSO_DATA_ID . ' -->';	// Since WPSSO Core v8.35.0.
 
-					$mt_mark_preg = $this->p->head->get_mt_data( 'begin-end-preg' );	// Since WPSSO Core v8.35.0.
+					$mt_mark_preg = $this->p->head->get_mt_data( 'preg' );	// Since WPSSO Core v8.35.0.
 
 					$mt_mark_matched = preg_match( $mt_mark_preg, $doc[ 'head' ], $matches, PREG_OFFSET_CAPTURE );
 
