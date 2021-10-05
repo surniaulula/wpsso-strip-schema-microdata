36b5de901739385e3f0789afb5dde74e60e9487a
JS Morisset <jsm@surniaulula.com>
2021-10-05 08:11:51 -0700

    v2.12.1-b.1

--- lib/submenu/ssm-general.php
+++ lib/submenu/ssm-general.php
@@ -92,10 +92,9 @@ if ( ! class_exists( 'WpssoSsmSubmenuSsmGeneral' ) && class_exists( 'WpssoAdmin'
 
 				$filter_name = SucomUtil::sanitize_hookname( 'wpsso_' . $metabox_id . '_' . $tab_key . '_rows' );
 
-				$table_rows[ $tab_key ] = array_merge(
-					$this->get_table_rows( $metabox_id, $tab_key ),
-					(array) apply_filters( $filter_name, array(), $this->form )
-				);
+				$table_rows[ $tab_key ] = $this->get_table_rows( $metabox_id, $tab_key );
+
+				$table_rows[ $tab_key ] = apply_filters( $filter_name, $table_rows[ $tab_key ], $this->form, $network = false );
 			}
 
 			$this->p->util->metabox->do_tabbed( $metabox_id, $tabs, $table_rows );
