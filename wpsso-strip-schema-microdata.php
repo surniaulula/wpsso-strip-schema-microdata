3aba4c27740fff6df397c26059498bdaae1319b7
JS Morisset <jsm@surniaulula.com>
2022-01-06 15:43:36 -0800

    v2.12.2

--- wpsso-strip-schema-microdata.php
+++ wpsso-strip-schema-microdata.php
@@ -13,7 +13,7 @@
  * Description: Remove Schema Microdata and RDFa from the webpage for better Google Rich Results using Schema JSON-LD markup.
  * Requires PHP: 7.2
  * Requires At Least: 5.2
- * Tested Up To: 5.8.2
+ * Tested Up To: 5.8.3
  * Version: 2.12.2
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
@@ -23,7 +23,7 @@
  *      {bugfix}        Backwards-compatible bug fixes or small improvements.
  *      {stage}.{level} Pre-production release: dev < a (alpha) < b (beta) < rc (release candidate).
  * 
- * Copyright 2016-2021 Jean-Sebastien Morisset (https://wpsso.com/)
+ * Copyright 2016-2022 Jean-Sebastien Morisset (https://wpsso.com/)
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
