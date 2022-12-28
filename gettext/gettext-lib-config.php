468e4973f0ec2b49b9126dc3ec16e2fc6a468f2d
JS Morisset <jsm@surniaulula.com>
2022-12-28 12:08:53 -0800

    v2.13.1

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -1,8 +1,7 @@
 <?php die( 'These aren\'t the droids you\'re looking for.' );
-
-/**
- * Extract of translatable strings from lib/config.php.
- */
+// 
+// Extract of translatable strings from lib/config.php.
+// 
 _x( 'Strip Microdata', 'lib file description', 'wpsso-strip-schema-microdata' );
 _x( 'WPSSO Strip Schema Microdata', 'plugin name', 'wpsso-strip-schema-microdata' );
 _x( 'Remove Schema Microdata and RDFa from the webpage for better Google Rich Results using Schema JSON-LD markup.', 'plugin description', 'wpsso-strip-schema-microdata' );
