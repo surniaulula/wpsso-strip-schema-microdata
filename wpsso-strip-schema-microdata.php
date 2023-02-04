d0eb9edf0cc4a3b278e666afffdcb9a09161d74f
JS Morisset <jsm@surniaulula.com>
2023-02-04 08:11:04 -0800

    v2.15.1

--- wpsso-strip-schema-microdata.php
+++ wpsso-strip-schema-microdata.php
@@ -67,6 +67,9 @@ if ( ! class_exists( 'WpssoSsm' ) ) {
 			load_plugin_textdomain( 'wpsso-strip-schema-microdata', false, 'wpsso-strip-schema-microdata/languages/' );
 		}
 
+		/*
+		 * Called by Wpsso->set_objects which runs at init priority 10.
+		 */
 		public function init_objects() {
 
 			$this->p =& Wpsso::get_instance();
